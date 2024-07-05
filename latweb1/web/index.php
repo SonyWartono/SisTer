dd822fcd361a50bb6291940db1eae4076b500914
SonyWartono <127180068+SonyWartono@users.noreply.github.com>
2024-07-05 09:16:22 +0800

    Update index.php

--- latweb1/web/index.php
+++ latweb1/web/index.php
@@ -4,47 +4,47 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan Docker</title>
+    <style>
+        body {
+        font-family: courier, courier-new;
+        margin: 0;
+        padding: 0;
+        background-color: #f4f4f4;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+    
+    .container {
+        background-color: white;
+        padding: 20px;
+        border-radius: 8px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        width: 300px;
+    }
+    
+    .container h1 {
+        text-align: center;
+        margin-bottom: 20px;
+    }
+    
+    table {
+        width: 100%;
+    }
+    
+    td {
+        padding: 5px 0;
+    }
+    
+    .label {
+        text-align: right;
+        padding-right: 10px;
+        font-weight: bold;
+    }
+    
+    </style>
 </head>
-<style>
-    body {
-    font-family: courier, courier-new;
-    margin: 0;
-    padding: 0;
-    background-color: #f4f4f4;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-}
-
-.container {
-    background-color: white;
-    padding: 20px;
-    border-radius: 8px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    width: 300px;
-}
-
-.container h1 {
-    text-align: center;
-    margin-bottom: 20px;
-}
-
-table {
-    width: 100%;
-}
-
-td {
-    padding: 5px 0;
-}
-
-.label {
-    text-align: right;
-    padding-right: 10px;
-    font-weight: bold;
-}
-
-</style>
 <body>
     <div class="container">
         <h1>Identitas</h1>
